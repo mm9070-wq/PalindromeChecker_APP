9d9f1db314efce7b76f3c8714ccd4ecff85edb74
mm9070-wq <mm9070@srmist.edu.in>
2026-03-17 22:30:52 +0530

    uc2

---
+++ UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+public class UseCase2PalindromeCheckerApp {
+
+    // Main method - Entry point
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String input = "madam";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (input.equals(reversed)) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
